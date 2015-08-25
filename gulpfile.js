2f77a786137b9944b112caf7426329fdddebfd90
kotorieclair <kotorieclair@gmail.com>
2015-08-25 17:39:58 +0900

    translate hbs to jade (still in progress)

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var kss = require('kss');
+var kssGenerator = require('./src/kss_jade_react_generator.js');
+var open = require('gulp-open')
+
+gulp.task('kss', function() {
+  kss.traverse('./example', {}, function(err, styleguide) {
+    kssGenerator.init({
+      source: ['./example'],
+      destination: './styleguide',
+      template: './src/template',
+      helpers: []
+    });
+
+    kssGenerator.generate(styleguide);
+  });
+});
+
+gulp.task('kss:open', function() {
+  return gulp.src('./styleguide/index.html')
+  .pipe($.open());
+});
+
+gulp.task('watch', ['kss'], function() {
+  gulp.watch(['./src/**/*', './example/**/*'], ['kss']);
+});
---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./kss_jade_react_generator.js');
